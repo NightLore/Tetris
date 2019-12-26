77721fefbae5a1d0ff54b6f021bb153f680d9405
Nathan Lui <nathanlui2010@gmail.com>
2019-12-25 19:20:40 -0800

    Framework for pieces

--- js/game.js
+++ js/game.js
@@ -1,2 +1,75 @@
 
+var Pieces = {
+    I: [[' ', ' ', ' ', ' '],
+        ['X', 'X', 'X', 'X'],
+        [' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ']],
+    O: [['X', 'X'],
+        ['X', 'X']],
+    T: [[' ', 'X', ' '],
+        ['X', 'X', 'X'],
+        [' ', ' ', ' ']],
+    S: [[' ', 'X', 'X'],
+        ['X', 'X', ' '],
+        [' ', ' ', ' ']],
+    Z: [['X', 'X', ' '],
+        [' ', 'X', 'X'],
+        [' ', ' ', ' ']],
+    J: [['X', ' ', ' '],
+        ['X', 'X', 'X'],
+        [' ', ' ', ' ']],
+    L: [[' ', ' ', 'X'],
+        ['X', 'X', 'X'],
+        [' ', ' ', ' ']],
 
+    getAllPieces: function() {
+        return [I, O, T, S, Z, J, L];
+    },
+
+    getRandomPiece: function() {
+        var pieces = getAllPieces();
+        return pieces[Math.random() * pieces.length];
+    },
+
+    rotateLeft: function(piece) {
+    },
+
+    rotateRight: function(piece) {
+    }
+}
+var Piece = function(p) {
+    this.piece = p || Pieces.getRandomPiece();
+}
+
+Piece.prototype.rotateLeft = function() {
+    var temp;
+    var size = this.piece.length;
+    var limit = Math.floor(size / 2);
+    for (var row = 0; row < limit; row++) {
+        for (var col = 0; col < limit; col++) {
+            temp = this.piece[row][col];
+            this.piece[row][col] = this.piece[row][size-1-col];
+            this.piece[row][size-1-col] = this.piece[size-1-row][size-1-col];
+            this.piece[size-1-row][size-1-col] = this.piece[size-1-row][col];
+            this.piece[size-1-row][col] = temp;
+        }
+    }
+}
+
+Piece.prototype.rotateRight = function() {
+    var temp;
+    var size = this.piece.length;
+    var limit = Math.floor(size / 2);
+    for (var row = 0; row < limit; row++) {
+        for (var col = 0; col < limit; col++) {
+            temp = this.piece[row][col];
+            this.piece[row][col] = this.piece[size-1-row][col];
+            this.piece[size-1-row][col] = this.piece[size-1-row][size-1-col];
+            this.piece[size-1-row][size-1-col] = this.piece[row][size-1-col];
+            this.piece[row][size-1-col] = temp;
+        }
+    }
+}
+
+Piece.prototype.draw = function(ctx) {
+}
